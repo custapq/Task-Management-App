6de689891e8755bc95195dee6c139d3967cd2750
custapq <teeradon88@gmail.com>
2025-04-28 18:15:58 +0700

    reduce header size in register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -4,7 +4,7 @@ const Register = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow-md">
-        <h1 className=" text-5xl font-bold text-center text-gray-900">Task Management Application</h1>
+        <h1 className=" text-4xl font-bold text-center text-gray-900">Task Management Application</h1>
         <h2 className="text-3xl font-bold text-center text-gray-900">Register</h2>
         <RegisterForm />
       </div>
